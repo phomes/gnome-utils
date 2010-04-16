6db37bf44daa7e2d9163dd98cdc0ef623d0efac0
Vincent Untz <vuntz@gnome.org>
2010-04-16 15:43:00 -0400

    [gsettings-tutorial] Make the application use the gconf backend
    To have a smooth migration for the code, we want to have GSettings and
    gconf coexist during the port. There's a GSettings backend that uses
    gconf for storage. This makes it possible to have one part of the code
    using GSettings while still allowing another part of the code to use
    gconf for the same key.
    
    A few notes:
    
      + This backend is only available in gconf >= 2.31.1.
    
      + This should be removed once the port is over and migration to the
        default GSettings backend is ready.
    
      + GSettings won't work unless there's a schema installed for the
        settings that will get accessed.

--- baobab/src/baobab.c
+++ baobab/src/baobab.c
@@ -1096,6 +1096,8 @@ main (int argc, char *argv[])
 	GOptionContext *context;
 	GError *error = NULL;
 
+	g_setenv ("GSETTINGS_BACKEND", "gconf", FALSE);
+
 	bindtextdomain (GETTEXT_PACKAGE, GNOMELOCALEDIR);
 	bind_textdomain_codeset (GETTEXT_PACKAGE, "UTF-8");
 	textdomain (GETTEXT_PACKAGE);
